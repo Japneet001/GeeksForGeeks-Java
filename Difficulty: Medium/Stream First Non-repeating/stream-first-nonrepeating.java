f3ff677bed44726646219f40828b26736a67e000
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2026-01-29 16:40:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String firstNonRepeating(String s) {
+        StringBuilder ans = new StringBuilder();
+        int[] freq = new int[26];
+        Queue<Character> q = new LinkedList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            freq[ch - 'a']++;
+            q.offer(ch);
+
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.poll();
+            }
+
+            if (q.isEmpty())   ans.append('#');
+            else   ans.append(q.peek());
+        }
+        return ans.toString();
+    }
+}
