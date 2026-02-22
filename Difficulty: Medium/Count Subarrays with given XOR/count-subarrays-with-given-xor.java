887d9b36dfe966b6147e0c5aa7194f75fdc4714c
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2026-02-22 22:58:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+        int prefixXor = 0;
+
+        for (int num : arr) {
+            prefixXor ^= num;
+
+            // Case 1: subarray from start has XOR = k
+            if (prefixXor == k) {
+                count++;
+            }
+
+            // Case 2: subarray ending here has XOR = k
+            int target = prefixXor ^ k;
+            if (freq.containsKey(target)) {
+                count += freq.get(target);
+            }
+
+            // Update frequency of prefixXor
+            freq.put(prefixXor, freq.getOrDefault(prefixXor, 0) + 1);
+        }
+
+        return count;
+
+    }
+}
