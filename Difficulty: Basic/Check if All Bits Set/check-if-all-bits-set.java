5052cb4a8cbf14961193dedc56d4f6bf26fd0831
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2026-05-21 22:19:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check if All Bits Set/check-if-all-bits-set.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isBitSet(int n) {
+        // code here
+        if(n == 0) return false;
+        while(n > 0){
+            int bit = n & 1;
+            if(bit == 0) return false;
+            n = n >> 1;
+        }
+        return true;
+    }
+};
