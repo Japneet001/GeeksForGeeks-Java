5cb7772487c9e2eee4f421fe4ee5976f359cb685
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2026-05-25 23:23:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.java
@@ -0,0 +1,13 @@
+import java.util.stream.Collectors;
+
+class Solution {
+    public boolean checkElements(int start, int end, int[] arr) {
+        // code here
+         return Arrays
+                .stream(arr)
+                .filter(a -> a >= start && a <= end)
+                .boxed()
+                .collect(Collectors.toSet())
+                .size() == (end - start + 1);
+    }
+}
